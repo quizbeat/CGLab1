d46b2c27015ab8f3315aaa7d311eb9faa02608f9
quizbeat <air@macbook.dlink>
2014-10-25 01:58:55 +0400

    Changed NMVector and NMMatrix classes

--- nmvector.h
+++ nmvector.h
@@ -7,9 +7,9 @@ public:
     float x, y, z, w;
     NMVector();
     NMVector(float _x, float _y, float _z, float _w);
-    NMVector operator+(const NMVector v);
-    NMVector operator-(const NMVector v);
-    void operator=(const NMVector v);
+    NMVector operator+(const NMVector &v);
+    NMVector operator-(const NMVector &v);
+    void operator=(const NMVector &v);
 };
 
 #endif // NMVECTOR_H
